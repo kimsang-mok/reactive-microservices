283c470e0cb4a273ed04054ddab8bdd28e39209d
Kimsang Mok <kimsang.mok01@gmail.com>
2024-07-26 11:43:13 +0700

    add: update review service and configuration

--- api/src/main/java/com/kimsang/api/composite/ServiceAddresses.java
+++ api/src/main/java/com/kimsang/api/composite/ServiceAddresses.java
@@ -9,4 +9,13 @@ import lombok.NoArgsConstructor;
 public class ServiceAddresses {
   private final String cmp;
   private final String pro;
+  private final String rev;
+  private final String rec;
+
+  public ServiceAddresses() {
+    cmp = null;
+    pro = null;
+    rev = null;
+    rec = null;
+  }
 }
